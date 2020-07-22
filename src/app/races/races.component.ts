f77a40cc7c2308bc8ca4d52a610d3d321b819550
tbrunton <thomas.brunton@ryerson.ca>
2020-07-21 20:02:25 -0400

    Edited races to use the general table component

--- src/app/races/races.component.ts
+++ src/app/races/races.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { DndapiService } from '../dndapi.service';
+import { TableComponent } from './../table/table.component';
 
 @Component({
   selector: 'app-races',
@@ -8,6 +9,8 @@ import { DndapiService } from '../dndapi.service';
   styleUrls: ['./races.component.css']
 })
 export class RacesComponent implements OnInit {
+  public caption = 'List of Races';
+  public headers: string[];
   public races: JSON[];
 
   constructor(private dndapiService: DndapiService) { }
@@ -20,6 +23,10 @@ export class RacesComponent implements OnInit {
     this.dndapiService.getRaces()
       .subscribe(races => {
         this.races = races['results'];
+        for(let race of this.races) {
+          this.headers = Object.keys(race);
+          break;
+        }
         //console.log(this.races);
       });
   }
