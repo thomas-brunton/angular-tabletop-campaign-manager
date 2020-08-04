1b0c5bf106be69927ffc71e5190eafe43577a80f
Tom Brunton <55635489+thomas-brunton@users.noreply.github.com>
2020-08-04 15:25:01 -0400

    Revert "Vtm 5e implementation"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MessagesComponent } from './messages/messages.component';
-import { RacesComponent } from './races/races.component';
-import { AppRoutingModule } from './app-routing.module';
-import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    MessagesComponent,
-    RacesComponent,
-    TableComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
