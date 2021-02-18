8b4d7c114e8caeb5d78bb47a801db0e64df4beb2
Ryan Hoang <tszfungryan.hoang@ryerson.ca>
2021-02-18 14:34:35 -0500

    added increased testing and error handling

--- src/app/api_services/vtmapi.service.ts
+++ src/app/api_services/vtmapi.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './apiInterface';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
 import { MessageService } from '../message.service';
 
 
@@ -31,6 +32,33 @@ export class VtmApiService implements ApiService{
   }
 
   sendRequest(url: string ): Observable<JSON[]>{
-    return this.http.get<JSON[]>(this.vtmApiUrl + url);
+    return this.http.get<JSON[]>(this.vtmApiUrl + url).pipe(
+      tap(() => this.log('fetched ' + url)),
+        catchError(this.handleError<JSON[]>('get' + url)) // TODO: figure out how to have a blank json object to send to error function
+    );
   }
+
+    /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation, result?: T) { // Maybe add back in operation = 'operation' if needed later for other http requests
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error);
+
+      // TODO: better job of transforming error for user consuption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result);
+    };
+  }
+
+  private log(message: string): void {
+    this.messageService.add(`DnDApi Service: ${message}`);
+  }
+
 }
